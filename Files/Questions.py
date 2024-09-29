e341f95d1e0de5d404292e906d158beb10bfe6d8
George Cooke <georgecooke8809@gmail.com>
2024-09-29 21:35:22 +0100

    Added Computer Science Papers Page

--- Files/Questions.py
+++ Files/Questions.py
@@ -15,24 +15,37 @@ Background = "White"
 Contrast = "#01104d"
 Contrast_Light = "#bfd2ff"
 
+select_window_frame = Frame(select)
+
+first_run = 0
+
 def inop(): #for when a button isn't programmed yet
     return ctypes.windll.user32.MessageBoxW(0, "INOP", "INOP", 4)
 
 def select_subject(): #select subject page for going into questions
-    select.title("Select Subject")
+    global subjects_main_f, subjects_f,papers_main_f
 
-    main_f = Frame(select) #blue frame on canva
-    main_f.rowconfigure(0, weight = 100)
-    main_f.rowconfigure(1, weight = 100)
-    main_f.rowconfigure(2, weight = 375, minsize = 495)
+    clear_page()
 
-    main_f.columnconfigure(0, weight = 1)
-    main_f.columnconfigure(1, weight = 1)
-    main_f.columnconfigure(2, weight = 1)
-    main_f.columnconfigure(3, weight = 1)
-    main_f.columnconfigure(4, weight = 1)
+    select.title("Select Subject:")
 
-    back = Button(main_f,
+    subjects_main_f = Frame(select_window_frame) #blue frame on canva
+    subjects_main_f.rowconfigure(0, weight = 1, minsize = 75)
+    subjects_main_f.rowconfigure(1, weight = 1, minsize = 75)
+    subjects_main_f.rowconfigure(2, weight = 375, minsize = 495)
+    subjects_main_f.rowconfigure(3, weight = 0)
+    subjects_main_f.rowconfigure(4, weight = 0)
+
+    subjects_main_f.columnconfigure(0, weight = 1)
+    subjects_main_f.columnconfigure(1, weight = 1)
+    subjects_main_f.columnconfigure(2, weight = 1)
+    subjects_main_f.columnconfigure(3, weight = 1)
+    subjects_main_f.columnconfigure(4, weight = 1)
+
+    papers_main_f = Frame(select_window_frame)
+    papers_main_f.pack()
+
+    back = Button(subjects_main_f,
                   text = "<- BACK",
                   command = inop,
                   font = (Font_1, 25, "bold"),
@@ -44,7 +57,7 @@ def select_subject(): #select subject page for going into questions
                   cursor = "hand2"
                   ).grid(row = 0, column = 0, sticky = N+E+S+W, pady = 10, padx = 10)
 
-    title = Label(main_f,
+    title = Label(subjects_main_f,
                   text = "Select Subject:",
                   font = (Font_1, 65, "bold"),
                   anchor = "w",
@@ -54,7 +67,7 @@ def select_subject(): #select subject page for going into questions
                   justify = "left"
                   ).grid(row = 1, column = 0, columnspan = 5, sticky = N+E+S+W)
 
-    subjects_f = Frame(main_f)#green frame on canve
+    subjects_f = Frame(subjects_main_f)#green frame on canve
     subjects_f.rowconfigure(0, weight = 1)
     subjects_f.rowconfigure(1, weight = 1)
     subjects_f.rowconfigure(2, weight = 1)
@@ -67,7 +80,7 @@ def select_subject(): #select subject page for going into questions
     #subject buttons:
     computer_science = Button(subjects_f,
                               text = "COMPUTER SCIENCE",
-                              command = inop,
+                              command = computer_science_papers,
                               font = (Font_1, 25, "bold"),
                               background = Contrast,
                               fg = "White",
@@ -155,7 +168,7 @@ def select_subject(): #select subject page for going into questions
 
     all_button = Button(subjects_f,
                               text = "ALL",
-                              command = inop,
+                              command = clear_page,
                               font = (Font_1, 37, "bold"),
                               background = Contrast,
                               fg = "White",
@@ -165,7 +178,100 @@ def select_subject(): #select subject page for going into questions
                               ).grid(row = 3, rowspan = 2, column = 1, sticky = N+E+S+W, pady = 10, padx = 10)
 
     subjects_f.grid(row = 2, column = 0, columnspan = 5, sticky = N+E+S+W)
-    main_f.pack(fill = "both", expand = True)
+    subjects_main_f.pack(fill = "both", expand = True)
+    select_window_frame.pack(fill = "both", expand = "true")
+
+def clear_page():
+    global first_run
+
+    if first_run >= 1:
+        select_window_frame.pack_forget()
+        subjects_main_f.pack_forget()
+        subjects_f.pack_forget()
+        papers_main_f.pack_forget()
+
+    first_run = first_run + 1
+
+def computer_science_papers():
+    global papers_main_f
+
+    select.title("Select Paper:")
+
+    clear_page()
+
+    papers_main_f = Frame(select_window_frame) #blue frame on canva
+    papers_main_f.rowconfigure(0, weight = 1, minsize = 75)
+    papers_main_f.rowconfigure(1, weight = 1, minsize = 75)
+    papers_main_f.rowconfigure(2, weight = 100)
+    papers_main_f.rowconfigure(3, weight = 100)
+    papers_main_f.rowconfigure(4, weight = 100)
+
+    papers_main_f.columnconfigure(0, weight = 1)
+    papers_main_f.columnconfigure(1, weight = 1)
+    papers_main_f.columnconfigure(2, weight = 1)
+    papers_main_f.columnconfigure(3, weight = 1)
+    papers_main_f.columnconfigure(4, weight = 1)
+
+    back = Button(papers_main_f,
+                  text = "<- BACK",
+                  command = select_subject,
+                  font = (Font_1, 25, "bold"),
+                  background = Contrast,
+                  fg = "White",
+                  activebackground = Contrast_Light,
+                  pady = 10,
+                  border = 0,
+                  cursor = "hand2"
+                  ).grid(row = 0, column = 0, sticky = N+E+S+W, pady = 10, padx = 10)
+
+    title = Label(papers_main_f,
+                  text = "Select Paper:",
+                  font = (Font_1, 65, "bold"),
+                  anchor = "w",
+                  padx = 10,
+                  wrap = True,
+                  wraplength = 1000,
+                  justify = "left"
+                  ).grid(row = 1, column = 0, columnspan = 5, sticky = N+E+S+W)
+
+    all_papers = Button(papers_main_f,
+                  text = "ALL",
+                  command = inop,
+                  font = (Font_1, 25, "bold"),
+                  background = Contrast,
+                  fg = "White",
+                  activebackground = Contrast_Light,
+                  pady = 10,
+                  border = 0,
+                  cursor = "hand2"
+                  ).grid(row = 2, column = 0, columnspan = 5, sticky = N+E+S+W, pady = 10, padx = 10)
+
+    paper_1 = Button(papers_main_f,
+                  text = "PAPER 1",
+                  command = inop,
+                  font = (Font_1, 25, "bold"),
+                  background = Contrast,
+                  fg = "White",
+                  activebackground = Contrast_Light,
+                  pady = 10,
+                  border = 0,
+                  cursor = "hand2"
+                  ).grid(row = 3, column = 0, columnspan = 5, sticky = N+E+S+W, pady = 10, padx = 10)
+
+    paper_2 = Button(papers_main_f,
+                  text = "PAPER 2",
+                  command = inop,
+                  font = (Font_1, 25, "bold"),
+                  background = Contrast,
+                  fg = "White",
+                  activebackground = Contrast_Light,
+                  pady = 10,
+                  border = 0,
+                  cursor = "hand2"
+                  ).grid(row = 4, column = 0, columnspan = 5, sticky = N+E+S+W, pady = 10, padx = 10)
+
+    papers_main_f.pack(fill = "both", expand = True)
+    select_window_frame.pack(fill = "both", expand = True)
 
 select_subject()
 select.mainloop()
